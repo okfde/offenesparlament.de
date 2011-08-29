aecbe1ee86f4417883f64a0bf512e8b0f460cdd8
Friedrich Lindenberg <friedrich@pudo.org>
2011-08-29 10:04:56 +0200

    bt networks for possible easter eggs

--- offenesparlament/default_settings.py
+++ offenesparlament/default_settings.py
@@ -4,3 +4,9 @@ SECRET_KEY = 'no'
 SOLR_URL = 'http://127.0.0.1:8983/solr/parlament'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///parlament.db'
+
+#   Network = 193.17.224.0/19
+#Usable IPs = 193.17.224.1 to 193.17.255.254 for 8190
+# Broadcast = 193.17.255.255
+#   Netmask = 255.255.224.0
+#Wildcard Mask = 0.0.31.255
