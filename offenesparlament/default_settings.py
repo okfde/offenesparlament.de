add0d45607e4ed1d4e8af9a59710cad5bfb10825
Friedrich Lindenberg <friedrich@pudo.org>
2011-09-21 18:25:18 +0200

    settings arguments.

--- offenesparlament/default_settings.py
+++ offenesparlament/default_settings.py
@@ -5,8 +5,12 @@ SOLR_URL = 'http://127.0.0.1:8983/solr/parlament'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///parlament.db'
 
-#   Network = 193.17.224.0/19
-#Usable IPs = 193.17.224.1 to 193.17.255.254 for 8190
+# Network = 193.17.224.0/19
+# Usable IPs = 193.17.224.1 to 193.17.255.254 for 8190
 # Broadcast = 193.17.255.255
-#   Netmask = 255.255.224.0
-#Wildcard Mask = 0.0.31.255
+# Netmask = 255.255.224.0
+# Wildcard Mask = 0.0.31.255
+
+STAGING_URL = 'http://localhost:5000/pudo/parlament'
+MASTER_URL = 'http://webstore.thedatahub.org/pudo/parlamaster'
+FLATPAGES_ROOT = 'pages'
