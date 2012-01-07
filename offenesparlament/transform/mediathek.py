7bd476f60ab819b8857d8169bb43316edbb1eead
Friedrich Lindenberg <friedrich@pudo.org>
2012-01-07 12:29:19 +0100

    ping pong debugging.

--- offenesparlament/transform/mediathek.py
+++ offenesparlament/transform/mediathek.py
@@ -67,7 +67,8 @@ def merge_speech(engine, master, wp, session):
     Mediathek = sl.get_table(engine, 'mediathek')
     Speech = sl.get_table(engine, 'speech')
     sorter = lambda x: (int(x['top_nr']), int(x['speech_nr']))
-    med = sorted(sl.find(engine, Mediathek, wahlperiode=wp, meeting_nr=session),
+    med = sorted(sl.find(engine, Mediathek, wahlperiode=wp,
+        meeting_nr=str(session)),
             key=sorter)
 
     speech_idx = top_idx = 0
