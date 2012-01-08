a2d0190f8cd49ae28b68b66a8d7f9801ae2c482a
Friedrich Lindenberg <friedrich@pudo.org>
2012-01-08 17:33:16 +0100

    Indexes for ETL and production DB.

---
+++ offenesparlament/indexes.sql
@@ -0,0 +1,17 @@
+
+# ON ETL:
+
+CREATE INDEX abl_key ON ablauf (key, wahlperiode);
+CREATE INDEX idx_sw ON schlagwort (key, wahlperiode);
+CREATE INDEX idx_swid ON schlagwort (id);
+CREATE INDEX ref_sw ON referenz (ablauf_key, wahlperiode);
+CREATE INDEX bei_sw ON beitrag (fundstelle, urheber, ablauf_id);
+CREATE INDEX zuw_sw ON zuweisung (fundstelle, urheber, ablauf_id);
+CREATE INDEX pos_sw ON position (ablauf_id);
+
+# ON PROD:
+
+CREATE INDEX load_person_fp ON person (fingerprint);
+CREATE INDEX load_zitat ON zitat (sitzung_id, sequenz);
+CREATE INDEX load_debatte_url ON debatte (source_url);
+CREATE INDEX load_debatte_zit_zit ON debatte_zitat (debatte_id, zitat_id);
