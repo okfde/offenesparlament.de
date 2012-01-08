665f947d25a123cde93090191e21e6c27fbb1eee
Friedrich Lindenberg <friedrich@pudo.org>
2012-01-08 17:21:48 +0100

    Pass through a timeout argument and put email in user agent.

--- offenesparlament/load/fetch.py
+++ offenesparlament/load/fetch.py
@@ -7,14 +7,15 @@ import uuid, time
 log = logging.getLogger(__name__)
 
 UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'
+UA = 'OPA // Neuer Scraper; weniger Zugriffe - bitte nicht blocken! <friedrich.lindenberg@okfn.org>'
 
-def fetch(url):
-    url = url.replace('http://', 'https://')
+def fetch(url, timeout=2.0):
+    #url = url.replace('http://', 'https://')
     for x in range(10):
         try:
             body = requests.get(url, 
                 headers={'user-agent': UA},
-                timeout=2.0,
+                timeout=timeout,
                 config={'max_retries': 10},
                 verify=False)
             return body.content
@@ -24,18 +25,18 @@ def fetch(url):
             logging.exception(e)
             time.sleep(1)
 
-def fetch_stream(url):
-    data = fetch(url)
+def fetch_stream(url, timeout=2.0):
+    data = fetch(url, timeout=timeout)
     if data is None:
         return None
     return StringIO(data)
 
-def _xml(url):
-    d = fetch_stream(url)
+def _xml(url, timeout=2.0):
+    d = fetch_stream(url, timeout=timeout)
     return etree.parse(d) if d is not None else d
 
-def _html(url):
-    d = fetch_stream(url)
+def _html(url, timeout=2.0):
+    d = fetch_stream(url, timeout=timeout)
     return html.parse(d) if d is not None else d
 
 
