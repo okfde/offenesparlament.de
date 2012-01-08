837486d1f3cf8b4735adc5b8f0a59d50783f7119
Friedrich Lindenberg <friedrich@pudo.org>
2012-01-08 23:56:45 +0100

    more indexes.

--- offenesparlament/indexes.sql
+++ offenesparlament/indexes.sql
@@ -1,6 +1,9 @@
 
 # ON ETL:
 
+CREATE INDEX vote_psn ON abstimmung (person);
+CREATE INDEX vote_subj ON abstimmung (subject);
+
 CREATE INDEX abl_key ON ablauf (key, wahlperiode);
 CREATE INDEX idx_sw ON schlagwort (key, wahlperiode);
 CREATE INDEX idx_swid ON schlagwort (id);
@@ -8,6 +11,11 @@ CREATE INDEX ref_sw ON referenz (ablauf_key, wahlperiode);
 CREATE INDEX bei_sw ON beitrag (fundstelle, urheber, ablauf_id);
 CREATE INDEX zuw_sw ON zuweisung (fundstelle, urheber, ablauf_id);
 CREATE INDEX pos_sw ON position (ablauf_id);
+
+CREATE INDEX speech_url ON mediathek (speech_source_url);
+
+CREATE INDEX speech_ssw ON speech (sequence, sitzung, wahlperiode);
+
 
 # ON PROD:
 
