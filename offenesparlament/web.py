697dd4d695d6655d6f0857f17d77ab3356411f45
Friedrich Lindenberg <friedrich@pudo.org>
2012-12-02 21:07:16 +0100

    FIx date generation

--- offenesparlament/web.py
+++ offenesparlament/web.py
@@ -23,6 +23,9 @@ from offenesparlament import aggregates
 
 
 def render_sitemap(items, prio=0.8):
+    items_ = []
+    for item in items:
+        item['lastmod'] = min(item.get('lastmod'), datetime.now())
     res = make_response(render_template('sitemap.xml',
         items=items, prio=prio))
     res.headers['Content-Type'] = 'text/xml; charset=utf-8'
