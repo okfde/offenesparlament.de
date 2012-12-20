803c261037a287cebafb82733d566e78734600af
Friedrich Lindenberg <friedrich@pudo.org>
2012-12-20 15:28:14 +0100

    Clean up configuration file.

--- offenesparlament/default_settings.py
+++ offenesparlament/default_settings.py
@@ -1,19 +1,11 @@
 DEBUG = True
 SECRET_KEY = 'no'
 
-
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///parlament.db'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/parlament'
-
-# Network = 193.17.224.0/19
-# Usable IPs = 193.17.224.1 to 193.17.255.254 for 8190
-# Broadcast = 193.17.255.255
-# Netmask = 255.255.224.0
-# Wildcard Mask = 0.0.31.255
-
-#HTTP_PROXY = 'http://127.0.0.1:3128'
-
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/parlament2'
 SOLR_URL = 'http://127.0.0.1:8983/solr/parlament'
 ETL_URL = 'postgresql://localhost/parlament_etl'
-MASTER_URL = 'http://parlament:l4mmm3rt@webstore.openspending.org/parlament/parlamaster'
 FLATPAGES_ROOT = 'pages'
+
+NOMENKLATURA_PERSONS_DATASET = 'offenesparlament'
+NOMENKLATURA_TYPES_DATASET = 'offenesparlament-typen'
+
