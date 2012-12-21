ee56f7e6cf34f51709c0cab6f9a45a03f48965bb
Friedrich Lindenberg <friedrich@pudo.org>
2012-12-22 00:00:38 +0100

    Caching reference data mgmt is caching.

--- offenesparlament/data/lib/reference.py
+++ offenesparlament/data/lib/reference.py
@@ -1,6 +1,7 @@
 import logging
+from threading import Lock
 
-from nkclient import NKDataset
+from nkclient import NKDataset, NKValue
 from nkclient import NKNoMatch, NKInvalid
 
 from offenesparlament.core import app
@@ -9,6 +10,7 @@ log = logging.getLogger(__name__)
 
 DATASETS = {}
 VALUES = {}
+LOCK = Lock()
 
 class BadReference(Exception):
     pass
@@ -19,24 +21,38 @@ class InvalidReference(BadReference):
 def dataset(name):
     if not name in DATASETS:
         DATASETS[name] = NKDataset(name, api_key=app.config['NOMENKLATURA_API_KEY'])
+        for value in DATASETS[name].values():
+            VALUES[(name, value.value.lower())] = value
+        for link in DATASETS[name].links():
+            if link.is_invalid:
+                VALUES[(name, link.key.lower())] = \
+                    NKInvalid({'dataset': link.dataset, 'key': link.key})
+            elif link.is_matched:
+                VALUES[(name, link.key.lower())] = \
+                    NKValue(DATASETS[name], link.value)
     return DATASETS[name]
 
 def resolve(dataset_name, key):
-    if (dataset_name, key) not in VALUES:
-        resolver = dataset(dataset_name)
-        try:
-            obj = resolver.lookup(key)
-        except NKInvalid, inv:
-            obj = inv
-        except NKNoMatch, nm:
-            obj = nm
-        VALUES[(dataset_name, key)] = obj
-    obj = VALUES[(dataset_name, key)]
-    if isinstance(obj, NKInvalid):
-        raise InvalidReference()
-    if isinstance(obj, NKNoMatch):
-        raise BadReference()
-    return obj.value
+    LOCK.acquire()
+    try:
+        cache_tpl = (dataset_name, key.lower())
+        if cache_tpl not in VALUES:
+            resolver = dataset(dataset_name)
+            try:
+                obj = resolver.lookup(key)
+            except NKInvalid, inv:
+                obj = inv
+            except NKNoMatch, nm:
+                obj = nm
+            VALUES[cache_tpl] = obj
+        obj = VALUES[cache_tpl]
+        if isinstance(obj, NKInvalid):
+            raise InvalidReference()
+        if isinstance(obj, NKNoMatch):
+            raise BadReference()
+        return obj.value
+    finally:
+        LOCK.release()
 
 def resolve_type(key):
     return resolve(app.config['NOMENKLATURA_TYPES_DATASET'], key)
