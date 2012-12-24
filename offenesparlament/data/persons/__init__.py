fc813a529ccdbedfa571b68e3250e7dfb2bfeb77
Friedrich Lindenberg <friedrich@pudo.org>
2012-12-24 16:33:49 +0100

    Create a table for the side-incomes of mps.

--- offenesparlament/data/persons/__init__.py
+++ offenesparlament/data/persons/__init__.py
@@ -5,6 +5,7 @@ from offenesparlament.data.lib.refresh import Unmodified
 from offenesparlament.data.persons.scrape import scrape_index, scrape_mdb
 from offenesparlament.data.persons.resolve import make_fingerprint
 from offenesparlament.data.persons.load import load_person
+from offenesparlament.data.persons.clean_angaben import parse_angaben
 
 log = logging.getLogger(__name__)
 
@@ -12,6 +13,7 @@ def process_person(engine, indexer, url, force=False):
     try:
         data = scrape_mdb(engine, url, force=force)
         make_fingerprint(engine, data)
+        parse_angaben(engine, data)
 
         data = fetch_row(engine, 'person', mdb_id=data['mdb_id'])
         person = load_person(engine, data)
