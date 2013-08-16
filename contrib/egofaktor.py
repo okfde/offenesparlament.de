154203ac61012b81b9dc12e6ae3a1eb3eb738fa9
Friedrich Lindenberg <friedrich@pudo.org>
2013-08-16 11:31:41 +0200

    SPIEGEL ego factor analysis.

---
+++ contrib/egofaktor.py
@@ -0,0 +1,51 @@
+import dataset
+import re
+from collections import defaultdict
+from unicodedata import normalize as ucnorm, category
+
+egos = re.compile('(ich|mir|mein|meiner|meines|mich)')
+words = re.compile('\w{2,}')
+
+engine = dataset.connect('postgresql://localhost/parlament_etl')
+speech = engine['speech']
+
+def normalize(text):
+    if not isinstance(text, unicode):
+        text = unicode(text)
+    decomposed = ucnorm('NFKD', text)
+    filtered = []
+    for char in decomposed:
+        cat = category(char)
+        if char == "'" or cat.startswith('M') or cat.startswith('S'):
+            continue
+        elif cat.startswith('L') or cat.startswith('N'):
+            filtered.append(char)
+        else:
+            filtered.append(' ')
+    text = u''.join(filtered)
+    while '  ' in text:
+        text = text.replace('  ', ' ')
+    return ucnorm('NFKC', text).strip().lower()
+
+
+num_egos = defaultdict(int)
+num_words = defaultdict(int)
+
+for s in speech.find(type='speech'):
+    if s['sitzung'] < 191 or s['in_writing']:
+        continue
+    text = normalize(s['text'])
+    num_egos[s['fingerprint']] += len(egos.findall(text))
+    num_words[s['fingerprint']] += len(words.findall(text))
+    #print [num_egos, num_words]
+
+print "Done, saving..."
+egofaktor = engine['egos']
+egofaktor.delete()
+for fp in num_egos.keys():
+    egofaktor.upsert({
+        'fingerprint': fp,
+        'egos': num_egos[fp],
+        'words': num_words[fp]
+        }, ['fingerprint'])
+
